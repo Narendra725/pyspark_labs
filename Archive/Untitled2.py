c3212a053bfe8ac003e59603aecb4e0214ea50c5
Narendra725 <132801604+Narendra725@users.noreply.github.com>
2025-11-02 12:22:33 +0530

    Update and rename Untitled2.py to Archive/Untitled2.py

---
+++ Archive/Untitled2.py
@@ -0,0 +1,29 @@
+# #!/usr/bin/env python
+# # coding: utf-8
+
+# # In[1]:
+
+
+# import findspark
+# findspark.init()
+
+
+# # In[2]:
+
+
+# from pyspark.sql import SparkSession
+# spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+
+# # In[11]:
+
+
+# data_df=spark.read.format("json").load("file:/home/labuser/Downloads/688b97cc-73ed-4262-b3be-6ef274e26311_83d04ac6-cb74-4a96-a06a-e0d5442aa126_cars.json")
+# data_df.show()
+
+
+# # In[10]:
+
+
+
+
